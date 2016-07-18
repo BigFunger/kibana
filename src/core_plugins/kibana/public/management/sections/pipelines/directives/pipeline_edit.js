7f3545d851b9577c1bd056cd0c20bef780543e1b
Jim Unger <BigFunger@gmail.com>
2016-07-18 15:57:14 -0500

    Moved pipeline edit code

--- src/core_plugins/kibana/public/management/sections/pipelines/directives/pipeline_edit.js
+++ src/core_plugins/kibana/public/management/sections/pipelines/directives/pipeline_edit.js
@@ -2,6 +2,9 @@ import management from 'ui/management';
 import modules from 'ui/modules';
 import routes from 'ui/routes';
 import template from '../views/pipeline_edit.html';
+import IngestProvider from 'ui/ingest';
+import Pipeline from '../lib/pipeline';
+import saveTemplate from '../partials/_pipeline_save.html';
 import './pipeline_output';
 import './source_data';
 import './processor_ui_container';
@@ -24,8 +27,35 @@ app.directive('pipelineEdit', function () {
   return {
     restrict: 'E',
     template: template,
-    controller: function ($scope, $route) {
+    controller: function ($scope, $route, kbnUrl, Private, Notifier) {
+      const ingest = Private(IngestProvider);
+      const notify = new Notifier({ location: `Ingest Pipeline Setup` });
       $scope.pipeline = $route.current.locals.pipeline;
+
+      $scope.topNavOpts = {
+        pipeline: $scope.pipeline,
+        doSave: () => {
+          const pipeline = $scope.pipeline;
+
+          return ingest.pipeline.save(pipeline.model)
+          .then((result) => {
+            notify.info(`Pipeline '${pipeline.pipelineId}' saved!`);
+            $scope.kbnTopNav.close();
+          })
+          .catch(notify.error);
+        }
+      };
+
+      $scope.topNavMenu = [{
+        key: 'save',
+        template: saveTemplate,
+        description: 'Save Pipeline'
+      },
+      {
+        key: 'close',
+        description: 'Cancel Unsaved Changes',
+        run: function () { kbnUrl.change('/management/elasticsearch/pipelines', {}); }
+      }];
     }
   };
 });
--- src/core_plugins/kibana/public/management/sections/pipelines/edit.js
+++ src/core_plugins/kibana/public/management/sections/pipelines/edit.js
@@ -3,7 +3,7 @@ import routes from 'ui/routes';
 import IngestProvider from 'ui/ingest';
 import Pipeline from './lib/pipeline';
 import template from './views/edit_app.html';
-import saveTemplate from './partials/_pipeline_save.html';
+//import saveTemplate from './partials/_pipeline_save.html';
 import './directives/pipeline_edit';
 
 routes
@@ -20,35 +20,13 @@ routes
       })
      .catch(notify.error);
     }
-  },
-  controller: function ($scope, $route, kbnUrl, Private, Notifier) {
-    const ingest = Private(IngestProvider);
-    const notify = new Notifier({ location: `Ingest Pipeline Setup` });
-    $scope.pipeline = $route.current.locals.pipeline;
-
-    $scope.topNavOpts = {
-      pipeline: $scope.pipeline,
-      doSave: () => {
-        const pipeline = $scope.pipeline;
-
-        return ingest.pipeline.save(pipeline.model)
-        .then((result) => {
-          notify.info(`Pipeline '${pipeline.pipelineId}' saved!`);
-          $scope.kbnTopNav.close();
-        })
-        .catch(notify.error);
-      }
-    };
-
-    $scope.topNavMenu = [{
-      key: 'save',
-      template: saveTemplate,
-      description: 'Save Pipeline'
-    },
-    {
-      key: 'cancel',
-      description: 'Cancel Changes',
-      run: function () { kbnUrl.change('/management/elasticsearch/pipelines', {}); }
-    }];
+  }
+})
+.when('/management/elasticsearch/pipeline', {
+  template: template,
+  resolve: {
+    pipeline: function () {
+      return new Pipeline();
+    }
   }
 });
