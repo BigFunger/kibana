3c15c020455ac0538895a0f311650952e02001dc
Jim Unger <BigFunger@gmail.com>
2017-05-10 10:06:44 -0500

    Adds all selected example

--- src/core_plugins/testbed/public/testbed.js
+++ src/core_plugins/testbed/public/testbed.js
@@ -9,11 +9,20 @@ uiRoutes.when('/testbed', {
   controllerAs: 'testbed',
   controller: class TestbedController {
     constructor() {
+      this.disableChanges = false;
+
       this.myItems = [
         { id: 1, name: 'foo', isSelected: false },
         { id: 17, name: 'bar', isSelected: true },
         { id: 23, name: 'baz', isSelected: false }
       ];
+
+      this.areAllSelected = false;
+    }
+
+    onSelectAllChange = (itemId, newIsSelected) => {
+      this.myItems.forEach(item => item.isSelected = newIsSelected);
+      this.updateAreAllSelected();
     }
 
     onSelectChange = (itemId, newIsSelected) => {
@@ -23,6 +32,12 @@ uiRoutes.when('/testbed', {
       }
 
       this.message = `Setting isSelected = ${newIsSelected} for ${foundItem.name}`;
+      this.updateAreAllSelected();
+    }
+
+    updateAreAllSelected = () => {
+      const newAllSelected = this.myItems.reduce((result, item) => result && item.isSelected, true);
+      this.areAllSelected = newAllSelected;
     }
   }
 });
